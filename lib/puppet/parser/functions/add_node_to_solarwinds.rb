fe5fa509d266f40ac86473c4e84b83bfca0553bc
Nathan Powell <nathan@nathanpowell.org>
2015-08-06 15:52:30 -0400

    Adding a randomizer for the pooling engine

--- lib/puppet/parser/functions/add_node_to_solarwinds.rb
+++ lib/puppet/parser/functions/add_node_to_solarwinds.rb
@@ -6,28 +6,32 @@ require "net/http"
 # This is a custom function to add nodes to Solarwinds that aren't already there.
 module Puppet::Parser::Functions
   newfunction(:add_node_to_solarwinds) do |args|
-    username  = function_hiera(['solarwinds_functions::config::username'])
-    password  = function_hiera(['solarwinds_functions::config::password'])
-    queryurl  = function_hiera(['solarwinds_functions::config::queryurl'])
-    addurl    = function_hiera(['solarwinds_functions::config::addurl'])
-    community = function_hiera(['solarwinds_functions::config::community'])
-    nodename  = lookupvar('fqdn')
-    ipaddr    = lookupvar('ipaddress')
+    config = {}    
 
-    response = checkstatus(nodename, username, password, queryurl)
+    config["username"]  = function_hiera(['solarwinds_functions::config::username'])
+    config["password"]  = function_hiera(['solarwinds_functions::config::password'])
+    config["queryurl"]  = function_hiera(['solarwinds_functions::config::queryurl'])
+    config["addurl"]    = function_hiera(['solarwinds_functions::config::addurl'])
+    config["community"] = function_hiera(['solarwinds_functions::config::community'])
+    config["poolers"]   = function_hiera(['solarwinds_functions::config::poolers'])
+    config["engineid"]  = pollers.split(",").sample
+    config["nodename"]  = lookupvar('fqdn')
+    config["ipaddr"]    = lookupvar('fqdn')
+    
+    response = checkstatus(config)
 
     if response == '{"results":[]}'
-      addhost(nodename, ipaddr, username, password, addurl, community)
+      addhost(config)
     end
 
   end
 end
 
 # Reach out to the Solarwinds (Orion) API and ask if the host is present
-def checkstatus(nodename, username, password, queryurl)
+def checkstatus(config)
 
-  uri = URI.parse("#{queryurl}")
-  query = {"query" => "SELECT NodeID FROM Orion.Nodes WHERE NodeName=@name", "parameters" => {"name" => "#{nodename}"}}
+  uri = URI.parse("#{config["queryurl"]}")
+  query = {"query" => "SELECT NodeID FROM Orion.Nodes WHERE NodeName=@name", "parameters" => {"name" => "#{config["nodename"]}"}}
 
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = true
@@ -35,7 +39,7 @@ def checkstatus(nodename, username, password, queryurl)
   
   request = Net::HTTP::Post.new(uri.request_uri, initheader = {'Content-Type' =>'application/json'})
   request.body = query.to_json
-  request.basic_auth("#{username}", "#{password}")
+  request.basic_auth("#{config["username"]}", "#{config["password"]}")
 
   response = http.request(request)
 
@@ -43,14 +47,14 @@ def checkstatus(nodename, username, password, queryurl)
 end
 
 # If the host was not present in the checkstatus() method, then we add it
-def addhost(nodename, ipaddr, username, password, addurl, community)
+def addhost(config)
 
-  uri = URI.parse("#{addurl}")
-  node = { "EntityType" => "Orion.Nodes", "IPAddress" => "#{ipaddr}",
-    "Caption"=> "#{nodename}", "DynamicIP" => "False", "EngineID" => 2, 
+  uri = URI.parse("#{config["addurl"]}")
+  node = { "EntityType" => "Orion.Nodes", "IPAddress" => "#{config["ipaddr"]}",
+    "Caption"=> "#{config["nodename"]}", "DynamicIP" => "False", "EngineID" => "#{config["engineid"]}", 
     "Status" => 1, "UnManaged" => "False", "Allow64BitCounters" => "True", 
     "SysObjectID" => "", "MachineType" => "", "VendorIcon" => "", 
-    "ObjectSubType" => "SNMP", "SNMPVersion" => 2, "Community" => "#{community}",
+    "ObjectSubType" => "SNMP", "SNMPVersion" => 2, "Community" => "#{config["community"]}",
   }
 
   http = Net::HTTP.new(uri.host, uri.port)
@@ -59,7 +63,7 @@ def addhost(nodename, ipaddr, username, password, addurl, community)
 
   request = Net::HTTP::Post.new(uri.request_uri, initheader = {'Content-Type' =>'application/json'})
   request.body = node.to_json
-  request.basic_auth("#{username}", "#{password}")
+  request.basic_auth("#{config["username"]}", "#{config["password"]}")
 
   response = http.request(request)
 end
